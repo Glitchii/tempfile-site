b8c2cba384e1e1f70d7039f89856e4a8819ff097
Glitchii <hello@tempfile.site>
2022-05-16 23:54:19 +0100

    Reposition regex variables

--- assets/components.js
+++ assets/components.js
@@ -29,36 +29,42 @@ export const dateFromValue = str => {
                     str.endsWith('w') ? (new Date).setDate((new Date).getDate() + 7 * digit) :
                         str.endsWith('mo') ? (new Date).setMonth((new Date).getMonth() + digit) :
                             new Date(str).setMinutes(new Date(str).getMinutes() + 1));
-    
+
     return !date.getDate() ? null : date > new Date((new Date).setMonth((new Date).getMonth() + 1)) ? 1 : date < new Date().setMinutes(new Date().getMinutes() + .5) ? 0 : date;
 }
 
-export const findName = async name => S3.listObjectsV2({ Bucket: process.env.bucket, }).promise()
-    .then(data => {
-        const arr = data.Contents.filter(x => new RegExp(`^${name}(-\\d+)?\\${ext}\\.json$`, 'gi').test(x.Key)).sort((x, y) => {
-            let name1 = x.Key.replace(exp, ''), name2 = y.Key.replace(re, '');
-            return re.test(name1) || re.test(name2) ? 0 : name1 < name2 ? -1 : name1 > name2 ? 1 : 0;
-        });
+export const findName = async name => {
+    const re = new RegExp(/(?<!-\d+)\.\w+$/g), exp = new RegExp(`\\${ext}\\.json$`)
+    return S3.listObjectsV2({ Bucket: process.env.bucket, }).promise()
+        .then(data => {
+            const arr = data.Contents.filter(x => new RegExp(`^${name}(-\\d+)?\\${ext}\\.json$`, 'gi').test(x.Key)).sort((x, y) => {
+                let name1 = x.Key.replace(exp, ''), name2 = y.Key.replace(re, '');
+                return re.test(name1) || re.test(name2) ? 0 : name1 < name2 ? -1 : name1 > name2 ? 1 : 0;
+            });
 
-        return arr.length !== 0 ? arr[arr.length - 1].Key.replace(/\.[^\.]+?\.json$/g, '') : null;
-    }).catch(() => null);
+            return arr.length !== 0 ? arr[arr.length - 1].Key.replace(/\.[^\.]+?\.json$/g, '') : null;
+        }).catch(() => null);
+}
 
 export const chooseName = async (filename, ext) => {
-    let name = filename ? filename.trim().toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-_\.]/g, '') : null,
-        re = new RegExp(/(?<!-\d+)\.\w+$/g),
-        exp = new RegExp(`\\${ext}\\.json$`);
+    const name = filename ? filename.trim().toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-_\.]/g, '') : null;
 
     if (name && await findName(name)) {
-        let found = await findName(name);
+        const found = await findName(name);
         return (found.match(/-\d+$/g) ? `${found.slice(0, -2)}-${parseInt(found.slice(-1)) + 1}` : found + '-1');
     } else if (!name) {
-        let min = 1, max = 6, check = async () => {
-            let name2 = [...Array(Math.floor(Math.random() * (max - min + min) + 1))].map(i => (~~(Math.random() * 36)).toString(36)).join('').toLowerCase();
-            if (await findName(name2)) await check();
-            return name2;
-        }
+        const min = 1,
+            max = 6,
+            check = async () => {
+                const name2 = [...Array(Math.floor(Math.random() * (max - min + min) + 1))].map(i => (~~(Math.random() * 36)).toString(36)).join('').toLowerCase();
+                if (await findName(name2))
+                    await check();
+                return name2;
+            }
         return await check();
-    } else return name;
+    }
+
+    return name;
 }
 
 export const checkIP = (ips, ips2) => {
